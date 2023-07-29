eb7f40b826755c863f1f7ebd79bb60436e2a8d47
Mayukh Hazari <hazari.mayukh77@gmail.com>
2023-07-29 10:30:29 +0530

    Google OAuth and Credential added

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,44 @@
+import type { NextAuthOptions } from "next-auth";
+import Google from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "your username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "your password",
+        },
+      },
+      async authorize(credentials) {
+        //Normal email password authentication
+        const user = {
+          id: "42",
+          name: "John",
+          email: "john@gmail.com",
+          password: "password",
+        };
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
